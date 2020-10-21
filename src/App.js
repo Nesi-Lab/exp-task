ab352b79ce77979bfd06e289f52bc1d2e634659e
Julia Windham <jwindha1@MacBook-Pro-de-Julia.local>
2020-10-21 18:41:27 -0400

    add to heroku

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { tl } from './timelines/main'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import { getTurkUniqueId } from './lib/utils'
+// import { getTurkUniqueId } from './lib/utils'
 
 // conditionally load electron and psiturk based on MTURK config variable
 // const isElectron = !MTURK
@@ -19,19 +19,20 @@ import { getTurkUniqueId } from './lib/utils'
 //   }
 // })
 
-const isElectron = true
+const isElectron = false
 let ipcRenderer = false;
 let psiturk = false
 if (isElectron) {
   const electron = window.require('electron');
   ipcRenderer  = electron.ipcRenderer;
-} else {
-  // React errors on build if the eslint is not disabled here
-  /* eslint-disable */
-  window.lodash = _.noConflict()
-  psiturk = new PsiTurk(getTurkUniqueId(), '/complete')
-  /* eslint-enable */
-}
+} 
+// else {
+//   // React errors on build if the eslint is not disabled here
+//   /* eslint-disable */
+//   window.lodash = _.noConflict()
+//   psiturk = new PsiTurk(getTurkUniqueId(), '/complete')
+//   /* eslint-enable */
+// }
 
 class App extends React.Component {
   render() {
